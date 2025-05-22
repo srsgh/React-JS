f2e92b92bafdd17e03a4c06ca69cd25e96b93bda
srsgh <theshrutirawat@gmail.com>
2025-05-23 02:36:33 +0530

    Body & Card

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 const Header = () => {
   return (
     <div className="header">
@@ -22,10 +23,67 @@ const Header = () => {
     </div>
   );
 };
+
+const Search = () => {
+  return <>{"Search"}</>;
+};
+//inline css
+const ResterauntCard = () => {
+  return (
+    // <div
+    //   className="res-card"
+    //   style={{
+    //     width: "200px",
+    //     height: "300px",
+    //     backgroundColor: "antiquewhite",
+    //     margin: "5px",
+    //     padding: "7px",
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     alignContent: "center",
+    //   }}
+    // >
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://tse4.mm.bing.net/th?id=OIP.TnX-DRtsSpc6_g-seWiIIgHaFj&pid=Api&P=0&h=180"
+      />
+      <h3>Vishal Chat</h3>
+      <h5>Vishal Chat</h5>
+      <h5>Vishal Chat</h5>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <Search />
+      </div>
+      <div className="res-container">
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+        <ResterauntCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
